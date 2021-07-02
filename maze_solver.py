b488000472c4c2850f1f1ddcf5f642adc093d60b
Arjun S <83405143+Arjun-Ichiban@users.noreply.github.com>
2021-07-02 18:20:48 +0530

    Update the initalizeArray function in maze_solver.py

--- maze_solver.py
+++ maze_solver.py
@@ -49,18 +49,10 @@ parent = []
 def initializeArrays(rows, cols):
     global dist, parent
     dist=[[infinite for x in range(rows)] for y in range(cols)]
-    # for i in range(rows):
-    #     for j in range(cols):
-    #         dist[i][j] = infinite
-    
-
     parent=[[Cell(-1, -1) for x in range(rows)] for y in range(cols)]
-    # for i in range(rows):
-    #     for j in range(cols):
-    #         parent[i][j] = 
     
     
-
+#check if it is a valid cell
 def valid(x, y):
     return x>=0 and x<len(grid) and y>=0 and y<len(grid[0])
 
@@ -70,22 +62,28 @@ def solveMaze(rows, cols):
 
     dist[0][0] = 0
 
+    #Dijstra's algorithm using heap data structure
     heap = []
     heapq.heappush(heap, Distance(0, 0, 0))
 
     while(len(heap)):
+        #take the minimum distance
         min = heapq.heappop(heap)
 
+        #find it's adjacent cell
         for d in direction:
             x = min.x+d[0]
             y = min.y+d[1]
-
+ 
+            #if the adjacent cell is valid and it is a path check it's distance and new distance
             if(valid(x, y) and grid[x][y]==1):
+                #if the new distance is lesser than update the dist array and add it to the heap 
                 if(min.dis+1<dist[x][y]):
                     dist[x][y] = min.dis+1
                     parent[x][y] = Cell(min.x, min.y)
                     heapq.heappush(heap, Distance(x, y, dist[x][y]))
 
+    #Print the path if it exist's
     if(dist[rows-1][cols-1]==max):
         print("Path Not Found")
     else:
@@ -93,6 +91,8 @@ def solveMaze(rows, cols):
         x = rows-1
         y = cols-1
 
+        #to find the shortest path
+        #we are travelling from the ending point to the starting point using the parent array
         while(not(x==0 and y==0)):
             finalPath[x][y] = 1
             par = parent[x][y]
@@ -101,6 +101,7 @@ def solveMaze(rows, cols):
 
         finalPath[0][0] = 1
 
+        #print the finalpath array 
         for i in range(rows):
             for j in range(cols):
                 print(finalPath[i][j], end=" ")
